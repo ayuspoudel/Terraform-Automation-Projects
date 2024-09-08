b0cb4ef3017a3dd64b58b1dde92fb1b466fa7834
Ayush Poudel <ayushpoudel@usf.edu>
2024-09-08 19:05:55 -0400

    Add main.tf

---
+++ AWS-ec2-Instance-Automation/main.tf
@@ -0,0 +1,20 @@
+variable "ami_id_value" {
+  description = "value for the AMI"
+  default = ""
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+}
+variable "subnet_id_value" {
+  description = "value for subnet id"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "terraform-automation" {
+    ami = var.ami_id_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
+}
